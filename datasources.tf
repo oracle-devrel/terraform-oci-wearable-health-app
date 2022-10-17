c5c9cd5d3bde7844b90043188e6571628c3ee2b5
RahulMR42 <>
2022-10-17 16:57:26 +0530

    updated identity

--- datasources.tf
+++ datasources.tf
@@ -4,15 +4,7 @@
 data "oci_identity_availability_domains" "ADs" {
   compartment_id = var.tenancy_ocid
 }
-data "oci_core_images" "InstanceImageOCID" {
-  compartment_id           = var.compartment_ocid
-  operating_system         = var.instance_os
-  operating_system_version = var.linux_os_version
 
-  shape      = var.instance_shape
-  sort_by    = "TIMECREATED"
-  sort_order = "DESC"
-}
 
 # Gets home and current regions
 
